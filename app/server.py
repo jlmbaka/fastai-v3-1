af3f0e985b570070e1bb7fd3fa6377bd8b979fd8
jlmbaka <jeanlouis.mbaka@gmail.com>
2020-08-18 00:20:03 +0100

    Adds a branch for a bear classifier

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1OA_QD_lbctEwBXjJMQa6YfqBGT1ojURG'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['cloth_mask', 'face_shield', 'full_face', 'n95_respirator', 'surgical_mask']
 path = Path(__file__).parent
 
 app = Starlette()
